e9bc410c9c399f2afd0ecc7f26b21cd26d1a9610
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-11-17 19:46:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function stringChop(str, size) {
   // your code here
+	let chunkArray = [];
+    
+    while(str.length > 0) {
+        let chunk = str.slice(0, size);
+        str = str.slice(size);
+        chunkArray.push(chunk);
+    }
+
+    return chunkArray;
 }
 
 // Do not change the code below
